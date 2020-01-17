2d2e2e8b0c13a57a4fc1ed1572df6065a6d353b2
MARCHAL Simon <marchal.simon@gmail.com>
2020-01-17 19:29:28 +0100

    Improved function encapsulation

--- app.js
+++ app.js
@@ -2,50 +2,43 @@
 
 const socket = new WebSocket('wss://pubsub-edge.twitch.tv:443');
 const client_id = 'nxyyskr9bnxcy08abnfnhrmxvqtjht';
-const api_url = 'https://api.twitch.tv/helix';
 
-var user;
-var app_token;
 var debug_output = document.getElementById('debug');
-var channel_name;
-
 
 if (document.location.hash) {
     document.getElementById('landing').classList.add('d-none');
 
     // Getting the OAuth token
-    var search = /=([a-z0-9]+)&/;
-    var url_fragment = document.location.hash;
-    app_token = url_fragment.match(search)[1];
+    var app_token = document.location.hash.match(/=([a-z0-9]+)&/)[1];
     console.debug('App token:', app_token);
 
-    getUserID();
-    pubsub();
+    getUserID(app_token, (user_id) => {
+        pubSub(socket, app_token, user_id);
+    })
 } else {
     document.getElementById('connect').setAttribute('href', document.getElementById('connect').getAttribute('href').replace('<url>', document.location.href));
-    console.debug('Document location:', document.location.href);
-    console.info('Please login to continue.')
+    console.info('Please login to continue.');
 }
 
 // Retrieving the User ID
-function getUserID() {
+function getUserID(oauth_token, callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
-                user = JSON.parse(httpRequest.response).data[0];
+                var user = JSON.parse(httpRequest.response).data[0];
                 console.debug('User:', user);
                 debug_output.append('User ID: '+ user.id +' ('+ user.display_name +')\n');
-                return user;
+                callback(user.id);
             }
         }
     };
-    httpRequest.open('GET', api_url +'/users', true);
-    httpRequest.setRequestHeader('Authorization', 'Bearer '+ app_token);
+    httpRequest.open('GET', 'https://api.twitch.tv/helix/users', true);
+    httpRequest.setRequestHeader('Authorization', 'Bearer '+ oauth_token);
     httpRequest.send();
 }
 
-function pubsub() {
+function pubSub(socket, auth_token, channel_id) {
     // PING the server every 5 minutes
     var ping = window.setInterval(() => {
         console.log('Sending PING to the server...');
@@ -66,12 +59,13 @@ function pubsub() {
                 }
                 break;
             case 'MESSAGE':
-
+                console.log('The server has sent a message!');
+                break;
+            case 'PONG':
+                console.log('The server responded PONG to the request!');
                 break;
             default:
-                if (response.type == 'PONG') {
-                    console.log('The server responded PONG to the request!');
-                }
+                console.log('The server responded to the request!');
         }
     }
     socket.onopen = () => {
@@ -80,8 +74,15 @@ function pubsub() {
         socket.send(JSON.stringify({
             'type': 'LISTEN',
             'data': {
-                'topics': ['channel-bits-events-v1.'+ user.id],
-                'auth_token': app_token
+                'topics': ['channel-bits-events-v2.'+ channel_id],
+                'auth_token': auth_token
+            }
+        }));
+        socket.send(JSON.stringify({
+            'type': 'LISTEN',
+            'data': {
+                'topics': ['whispers.'+ channel_id],
+                'auth_token': auth_token
             }
         }));
     }
