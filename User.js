03825e3d689068259c6c8492a1b80062f42f1834
MARCHAL Simon <marchal.simon@gmail.com>
2020-05-12 14:40:14 +0200

    Using Promise for http requests

--- User.js
+++ User.js
@@ -4,38 +4,64 @@ class User {
     id;
     name;
     token;
+    clientId;
     totalFollows;
     lastFollower;
 
-    constructor(token) {
+    constructor(token, clientId) {
         this.token = token;
+        this.clientId = clientId;
     }
 
-    getUser(callback) {
-        this.httpRequest('https://api.twitch.tv/helix/users', (response) => {
-            this.id = response.data[0].id;
-            this.name = response.data[0].display_name;
-            callback();
+    getUser() {
+        return new Promise((resolve, reject) => {
+            this.httpRequest('https://api.twitch.tv/helix/users')
+            .then((response) => {
+                this.id = response.data[0].id;
+                this.name = response.data[0].display_name;
+                resolve(response);
+            })
+            .catch((response) => {
+                reject(response);
+            });
         });
     }
-    getLastFollow(callback) {
-        this.httpRequest('https://api.twitch.tv/helix/users/follows?first=1&to_id=' + this.id, (response) => {
-            this.totalFollows = response.total;
-            this.lastFollower = response.data[0].from_name;
-            callback();
+    getLastFollow() {
+        return new Promise((resolve, reject) => {
+            this.httpRequest('https://api.twitch.tv/helix/users/follows?first=1&to_id=' + this.id)
+            .then((response) => {
+                this.totalFollows = response.total;
+                this.lastFollower = response.data[0].from_name;
+                resolve(response);
+            })
+            .catch((response) => {
+                reject(response);
+            });
         });
     }
-    httpRequest(url, callback) {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = () => {
-            if (request.readyState === XMLHttpRequest.DONE) {
-                if (request.status === 200) {
-                    callback(JSON.parse(request.response));
+
+    httpRequest(url) {
+        return new Promise((resolve, reject) => {
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = () => {
+                if (request.readyState === XMLHttpRequest.DONE) {
+                    switch (request.status) {
+                        case 200:
+                            resolve(JSON.parse(request.response));
+                            break;
+                        default:
+                            reject(JSON.parse(request.response));
+                            break;
+                    }
                 }
-            }
-        };
-        request.open('GET', url, true);
-        request.setRequestHeader('Authorization', 'Bearer ' + this.token);
-        request.send();
+            };
+            request.onerror = () => {
+                reject(request.statusText);
+            };
+            request.open('GET', url, true);
+            request.setRequestHeader('Client-ID', this.clientId);
+            request.setRequestHeader('Authorization', 'Bearer ' + this.token);
+            request.send();
+        });
     }
 };
--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 'use strict';
 
 const socket = new WebSocket('wss://pubsub-edge.twitch.tv:443');
-const client_id = 'nxyyskr9bnxcy08abnfnhrmxvqtjht';
+const clientId = 'nxyyskr9bnxcy08abnfnhrmxvqtjht';
 
-var debug_output = document.getElementById('debug');
+const debug_output = document.getElementById('debug');
 
 if (document.location.hash) {
     document.getElementById('landing').classList.add('d-none');
 
-    var user = new User(document.location.hash.match(/access_token=(\w+)/)[1]);
+    const user = new User(document.location.hash.match(/access_token=(\w+)/)[1], clientId);
     console.debug('App token:', user.token);
 
-    user.getUser(() => {
+    user.getUser()
+    .then(() => {
         debug_output.append('User ID: '+ user.id +' ('+ user.name +')\n');
         var pubsub = new PubSub(socket, user.token, user.id);
         pubsub.start();
-        user.getLastFollow(() => {
+        user.getLastFollow()
+        .then(() => {
             debug_output.append('Total followers: '+ user.totalFollows +' (last: '+ user.lastFollower +')\n');
-        })
+        });
+    })
+    .catch((error) => {
+        console.error(error);
     });
-
 } else {
     debug_output.parentNode.parentNode.classList.add('d-none');
     document.getElementById('connect')
@@ -35,7 +39,7 @@ if (document.location.hash) {
                 'href',
                 document.getElementById('connect')
                         .getAttribute('href')
-                        .replace('<client_id>', client_id)
+                        .replace('<client_id>', clientId)
             )
     ;
     console.info('Please login to continue.');
