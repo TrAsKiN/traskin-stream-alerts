7a057fce585f57f776ea576afa4fbe8cac6ab1e6
TrAsKiN <marchal.simon@gmail.com>
2021-02-28 15:42:13 +0100

    Create a dashboard

--- app.js
+++ app.js
@@ -3,54 +3,75 @@
 const socket = new WebSocket('wss://pubsub-edge.twitch.tv:443');
 const clientId = 'nxyyskr9bnxcy08abnfnhrmxvqtjht';
 
-document.getElementById('landing').classList.add('d-none');
-
 if (window.localStorage.getItem('access_token')) {
-    // Hide login request
-    document.getElementById('overlay').classList.remove('d-none');
+    if (document.location.hash.slice(1) === 'dashboard') {
+        console.info('Welcome on the dashboard.');
 
-    // User creation with access token
-    const user = new User(window.localStorage.getItem('access_token'), clientId);
+        const user = new User(window.localStorage.getItem('access_token'), clientId);
 
-    // Retrieving user information with access token
-    user.getUser()
-    .then(() => {
-        console.debug(user);
-        // Retrieving the last follower and the total number of followers
-        user.getLastFollow()
-        .then(() => {
-            const followers = new Followers(user.lastFollower, user.totalFollowers);
-            window.setInterval(() => {
-                user.getLastFollow()
+        document.getElementById('dashboard').classList.remove('d-none');
+        document.getElementById('testFollow').addEventListener('click', (e) => {
+            e.preventDefault();
+            user.getUser()
                 .then(() => {
-                    if (user.lastFollower !== followers.lastFollowerName) {
-                        console.log('New follow!');
-                        followers.newFollow(user.lastFollower, user.totalFollowers);
-                    }
-                });
-            }, 1000);
-        })
-        .catch((error) => {
-            console.error(error);
+                    user.getLastFollow()
+                        .then(() => {
+                            const followers = new Followers(user.lastFollower, user.totalFollowers);
+                            console.log('Test follow!');
+                            followers.newFollow(user.lastFollower, user.totalFollowers);
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
+                })
+            ;
         });
+    } else {
+        // Hide login request
+        document.getElementById('overlay').classList.remove('d-none');
 
-        // Creating and starting the websocket
-        const pubsub = new PubSub(socket, user.token, user.id);
-        pubsub.start()
-        .then((response) => {
-            console.debug(response);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-    })
-    .catch((error) => {
-        console.error(error);
+        // User creation with access token
+        const user = new User(window.localStorage.getItem('access_token'), clientId);
 
-        // Deleting saved information and reloading
-        window.localStorage.clear();
-        document.location.reload();
-    });
+        // Retrieving user information with access token
+        user.getUser()
+            .then(() => {
+                // Retrieving the last follower and the total number of followers
+                user.getLastFollow()
+                    .then(() => {
+                        const followers = new Followers(user.lastFollower, user.totalFollowers);
+                        window.setInterval(() => {
+                            user.getLastFollow()
+                                .then(() => {
+                                    if (user.lastFollower !== followers.lastFollowerName) {
+                                        console.log('New follow!');
+                                        followers.newFollow(user.lastFollower, user.totalFollowers);
+                                    }
+                                });
+                        }, 1000);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+
+                // Creating and starting the websocket
+                const pubsub = new PubSub(socket, user.token, user.id);
+                pubsub.start()
+                    .then((response) => {
+                        console.debug(response);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            })
+            .catch((error) => {
+                console.error(error);
+
+                // Deleting saved information and reloading
+                window.localStorage.clear();
+                document.location.reload();
+            });
+    }
 } else if (document.location.hash) {
     if (document.location.hash.slice(1) === 'dashboard') {
         // Customization of the url with application information
